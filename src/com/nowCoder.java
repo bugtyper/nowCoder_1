98d9c9b95d266f55451ba9c6c0a542e97288b04a
bugtyper <2712410843@qq.com>
2020-01-05 13:23:50 +0800

    Initial commit

---
+++ src/com/nowCoder.java
@@ -0,0 +1,11 @@
+package com;
+
+public class nowCoder {
+
+    public static void main(String[] args) {
+        System.out.println("hello nowCoer1111!!!");
+
+        System.out.println("hello nowCoer2222!!!");
+        System.out.println();
+    }
+}
